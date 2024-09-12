e853ac57bb9dc881374508e023ac149448046658
kimbrian <118575968+kimbrianreyes@users.noreply.github.com>
2024-09-13 07:24:54 +0800

    Update User.php

--- csm/User.php
+++ csm/User.php
@@ -14,14 +14,12 @@ class User {
         if ($stmt->execute()) {
             return true;
         } else {
-            // Log or display error for debugging
             error_log("Error executing query: " . $stmt->error);
             return false;
         }
     }
     
     public function login($username, $password) {
-        // Query to fetch user by username
         $query = "SELECT * FROM users WHERE username = ?";
         $stmt = $this->conn->prepare($query);
         $stmt->bind_param("s", $username);
@@ -30,14 +28,13 @@ class User {
     
         if ($result->num_rows > 0) {
             $user = $result->fetch_assoc();
-            // Verify the hashed password
             if (password_verify($password, $user['password'])) {
-                return $user; // Password matched
+                return $user; 
             } else {
-                return false; // Password didn't match
+                return false;
             }
         } else {
-            return false; // No user found with the given username
+            return false; 
         }
     }
 }
